cee9e0866f670c35964f32cb9bc1c5da876c8f02
jzl1 <34795003+jzl1@users.noreply.github.com>
2021-12-12 22:39:24 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,12 +5,10 @@ wget -O /app/nezha "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/m
 wget -O /app/alwayson.sh "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/alwayson.sh"
 wget -O /app/bench "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/webBenchmark_linux_x64"
 wget -O /app/Caddyfile "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/Caddyfile"
-
-echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" \
-    | sudo tee -a /etc/apt/sources.list.d/caddy-fury.list
-apt update
-apt install caddy
-
+wget -O /app/caddy.zip "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/caddy.zip"
+cd /app
+unzip /app/caddy.zip
+cd /
 chmod +x /app/*
 nohup /app/nezha -s $tz_address -p $tz_secret --skip-conn --skip-procs &&
 nohup caddy run -config /app/Caddyfile &&
