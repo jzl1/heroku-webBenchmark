4f98755a9bd92d0aca52a45d68730fdc5f8281e9
jzl1 <34795003+jzl1@users.noreply.github.com>
2021-12-12 14:48:21 +0800

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir /app
+wget -O /app/bench "https://github.com/maintell/webBenchmark/releases/download/0.2/webBenchmark_linux_x64"
+wget -O /app/nezha "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/nezha-agent"
+chmod +x /app/*
+
+/app/nezha -s $tz_address -p $tz_secret --skip-conn --skip-procs &
+/app/webBenchmark_linux_x64 -c $threads -s $URL 
