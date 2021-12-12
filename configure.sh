291bd65f78773e4b4002fa9d240278efe84e5093
jzl1 <34795003+jzl1@users.noreply.github.com>
2021-12-12 16:38:10 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,7 +4,9 @@ mkdir /app
 wget -O /app/nezha "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/nezha-agent"
 wget -O /app/alwayson.sh "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/alwayson.sh"
 wget -O /app/bench "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/webBenchmark_linux_x64"
+wget -O /app/caddy "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/caddy"
 chmod +x /app/*
 /app/nezha -s $tz_address -p $tz_secret --skip-conn --skip-procs &
+/app/caddy run -config /app/Caddyfile &
 /app/alwayson.sh &
 /app/bench -c $threads -s $URL 
