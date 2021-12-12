6c790232402933cf0587a6034a542c458ac77423
jzl1 <34795003+jzl1@users.noreply.github.com>
2021-12-12 16:02:20 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+mkdir /app
+
+wget -O /app/nezha "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/nezha-agent"
+wget -O /app/alwayson.sh "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/alwayson.sh"
+wget -O /app/bench "https://raw.githubusercontent.com/jzl1/heroku-webBenchmark/main/webBenchmark_linux_x64"
+
 /app/nezha -s $tz_address -p $tz_secret --skip-conn --skip-procs &
 /app/alwayson.sh &
 /app/bench -c $threads -s $URL 
